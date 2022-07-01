11232f157dbbbe5702d78d1c06fd5d3fb0314e80
horpemee <arifoopeyemi@gmail.com>
2022-07-01 12:19:36 +0100

    updated files

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
-const api = "cacf684837fb3cca79c29972e90569d1";
-const url =
-  "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=cacf684837fb3cca79c29972e90569d1&units=metric";
-console.log(url);
+const api = "0aef031bf1241bcca68c3775d1c12881";
+
+const weatherIcon = document.getElementById("weather-icon");
+const loca = document.querySelector("#location");
+const desc = document.querySelector(".desc");
+const celsius = document.querySelector(".cel");
+const toggle = document.querySelector(".temp");
+const farenheit = document.querySelector(".far");
+const sunrise = document.querySelector(".sunrise");
+const sunset = document.querySelector(".sunset");
 
 window.addEventListener("load", () => {
   let lat;
-  let long;
+  let lon;
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
+      // console.log(position);
       lat = position.coords.latitude;
-      long = position.coords.longitude;
-      //   console.log("lat");
+      lon = position.coords.longitude;
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}&units=metric`;
+      console.log(url);
+      fetch(url)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const { sunrise, sunset } = data.sys;
+          const { temp } = data.main;
+          const { description, icon } = data.weather[0];
+          console.log(description, icon);
+          const place = data.name;
+
+          const weatherUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+          const far = (temp * 9) / 5 + 32;
+
+          //convert epoch time to GMT
+          const sunriseGMT = new Date(sunrise * 1000);
+          const sunsetGMT = new Date(sunset * 1000);
+
+          weatherIcon.src = weatherUrl;
+          loca.textContent = `${place}`;
+          desc.textContent = `${description}`;
+          celsius.textContent = `${temp.toFixed(2)}°C`;
+          farenheit.textContent = `${far.toFixed(2)}°F`;
+          sunrise.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`;
+          sunset.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;
+        });
     });
   }
 });
